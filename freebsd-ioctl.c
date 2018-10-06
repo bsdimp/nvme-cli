b490c911e7d306d0286671374b2f95c341cca149
Warner Losh <imp@FreeBSD.org>
2018-10-06 09:43:29 -0600

    more debugs for the moment in the freeBSD code

--- freebsd-ioctl.c
+++ freebsd-ioctl.c
@@ -16,6 +16,7 @@
 	errno = ENOTTY;					\
 	return -1;					\
 
+#define TRACE
 #undef ioctl
 
 static int
@@ -41,9 +42,23 @@ freebsd_submit_admin(int fd, unsigned long cmd, struct nvme_admin_cmd *arg)
 	pt.cmd.cdw15	= arg->cdw15;
 	pt.buf		= (void *)arg->addr;
 	pt.len		= arg->data_len;
-	pt.is_read	= (arg->opcode & 1) == 0;
+	pt.is_read	= arg->opcode & 1;
 	pt.driver_lock	= 0;
+
+#ifdef TRACE
+	fprintf(stderr, "%#08lx: opc: %#x fuse: %#x cid %#x nsid:%#x cmd2: %#x cmd3: %#x\n",
+	    cmd, pt.cmd.opc, pt.cmd.fuse, pt.cmd.cid, pt.cmd.nsid,
+	    pt.cmd.rsvd2, pt.cmd.rsvd3);
+	fprintf(stderr, "          : cdw10: %#x cdw11: %#x cdw12: %#x cdw13: %#x\n", 
+	    pt.cmd.cdw10, pt.cmd.cdw11, pt.cmd.cdw12, pt.cmd.cdw13);
+	fprintf(stderr, "          : cdw14: %#x cdw15: %#x len: %#x is_read: %d\n",
+	    pt.cmd.cdw14, pt.cmd.cdw15, pt.len, pt.is_read);
+#endif
+
 	rv = ioctl(fd, NVME_PASSTHROUGH_CMD, &pt);
+#ifdef TRACE
+	fprintf(stderr, "<--- %d cid: %#x status %#x\n", rv, pt.cpl.cid, pt.cpl.status);
+#endif
 	if (rv == -1)
 		return (rv);
 	arg->result	= htole16(pt.cpl.cid) | htole16(pt.cpl.status) << 16;
@@ -70,6 +85,7 @@ freebsd_ioctl(int fd, unsigned long cmd, ...)
 	XXX(NVME_IOCTL_SUBSYS_RESET);
 	XXX(NVME_IOCTL_RESCAN);
 	default:
+		printf("Passing through IOCTL %#lx\n", cmd);
 		return (ioctl(fd, cmd, arg));
 	}
 }
